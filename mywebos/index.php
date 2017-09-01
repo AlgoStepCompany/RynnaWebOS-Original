f146e5b8c566559cf86914ed9215c91b6535fced
AlgostepDG <loic.ab@outlook.fr>
2017-09-01 14:13:18 +0200

    Update index.php
    Version correction et amélioration 9.1

--- mywebos/index.php
+++ mywebos/index.php
@@ -227,7 +227,7 @@ $(document).ready(function()
 </div>
 </div>
 <div id="wb_Text10" style="position:absolute;left:7px;top:7px;width:250px;height:44px;z-index:21;">
-<span style="color:#000000;font-family:Arial;font-size:13px;"><em>Version 9.1 (Release)<br>Desktop Virtual Manager (DVM)<br></em></span><span style="color:#000000;font-family:Arial;font-size:9.3px;"><em>PHP 7, CSS 3, JQuery 3.1.1</em></span></div>
+<span style="color:#000000;font-family:Arial;font-size:13px;"><em>Version 9.1b (Release)<br>Desktop Virtual Manager (DVM)<br></em></span><span style="color:#000000;font-family:Arial;font-size:9.3px;"><em>PHP 7, CSS 3, JQuery 3.1.1</em></span></div>
 <div id="Layer1" style="position:absolute;text-align:center;left:277px;top:564px;width:393px;height:83px;z-index:22;">
 <div id="Layer1_Container" style="width:393px;position:relative;margin-left:auto;margin-right:auto;text-align:left;">
 </div>
