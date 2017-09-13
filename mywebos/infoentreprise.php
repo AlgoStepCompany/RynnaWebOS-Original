2bcf357d4d24976af97d8b512b9dcdc48672c551
AlgostepDG <loic.ab@outlook.fr>
2017-09-13 22:03:38 +0200

    Add files via upload

---
+++ mywebos/infoentreprise.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>RynnaWebOS</title>
+<meta name="generator" content="AlgoStep Company - 2006-2017">
+<link href="RynnaWebOS.css" rel="stylesheet">
+<link href="infoentreprise.css" rel="stylesheet">
+</head>
+<body>
+<div id="wb_Text1" style="position:absolute;left:6px;top:8px;width:393px;height:171px;z-index:0;">
+<span style="color:#000000;font-family:Arial;font-size:16px;"><strong>Si vous disposez du code source vous pouvez modifier le code de session.php et demosession.php pour cibler vos propres pages PHP (ApplisWeb) pour les liés à ce programme pour en créer un accès rapide aux applications de votre Entreprise.<br><br>Contactez votre Administrateur réseau pour de plus amples informations.</strong></span></div>
+</body>
+</html>
