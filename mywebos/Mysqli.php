ec25683cd4cc530bf95e927c0488d4c7c1552e62
AlgostepDG <loic.ab@outlook.fr>
2017-09-29 08:54:34 +0200

    Rename Mysql.php to Mysqli.php

--- mywebos/Mysqli.php
+++ mywebos/Mysqli.php
@@ -6,8 +6,8 @@ $password = '';
 $connect = mysqli_connect($host_name, $user_name, $password, $database);
 
 if (mysqli_connect_errno()) {
-    die('<p>La connexion au serveur MySQL a échoué: '.mysqli_connect_error().'</p>');
+    die('<p>La connexion au serveur MySQL a Ã©chouÃ©: '.mysqli_connect_error().'</p>');
 } else {
-    echo '<p>Connexion au serveur MySQL établie avec succès.</p >';
+    echo '<p>Connexion au serveur MySQL Ã©tablie avec succÃ¨s.</p >';
 }
 ?>
