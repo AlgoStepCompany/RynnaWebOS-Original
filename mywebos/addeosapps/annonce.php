4248ce46d368042aa2630fd3b4b37887e26167ec
AlgostepDG <loic.ab@outlook.fr>
2018-01-01 23:14:06 +0100

    Add files via upload
    20.3

---
+++ mywebos/addeosapps/annonce.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>RynnaWebOS</title>
+<meta name="generator" content="AlgoStep Company - 2006-2017">
+<link href="RynnaWebOS.css" rel="stylesheet">
+<link href="annonce.css" rel="stylesheet">
+</head>
+<body>
+<div id="Layer1" style="position:fixed;overflow:hidden;text-align:left;left:0;top:0;right:0;bottom:0;z-index:1;">
+<div id="wb_Text1" style="position:absolute;left:7px;top:11px;width:348px;height:120px;z-index:0;">
+<span style="color:#000000;font-family:'Trebuchet MS';font-size:15px;"><em>Bienvenue !<br>Editer le fichier &quot;annonce.php&quot; qui se trouve dans le dossier &quot;addeosapps&quot; pour afficher une annonce qui s'affichera sur chaque écran de tout les utilisteurs à l'ouverture de leurs sessions.<br>Plus d'infos sur la page officiel GitHub du projet.</em></span></div>
+</div>
+</body>
+</html>
