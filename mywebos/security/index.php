522ec9e5af6b7e2d9ed025c3befe8f67136d2950
AlgostepDG <loic.ab@outlook.fr>
2018-01-04 23:57:05 +0100

    Add files via upload
    21.0
    Préparation sécurité

---
+++ mywebos/security/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta charset="utf-8">
+<title>RynnaWebOS</title>
+<meta name="generator" content="AlgoStep Company - 2006-2017">
+<link href="RynnaWebOS.css" rel="stylesheet">
+<link href="index.css" rel="stylesheet">
+<script src="jquery-3.2.1.min.js"></script>
+<script src="wb.stickylayer.min.js"></script>
+<script>
+$(document).ready(function()
+{
+   $("#Layer1").stickylayer({orientation: 9, position: [0, 0], delay: 0});
+});
+</script>
+</head>
+<body>
+<div id="Layer1" style="position:absolute;text-align:center;left:19px;top:21px;width:536px;height:122px;z-index:2;">
+<div id="Layer1_Container" style="width:534px;position:relative;margin-left:auto;margin-right:auto;text-align:left;">
+<input type="button" id="Button2" name="" value="Accès non autorisé !" style="position:absolute;left:11px;top:10px;width:512px;height:25px;z-index:0;" disabled>
+<div id="wb_Text1" style="position:absolute;left:20px;top:52px;width:494px;height:35px;z-index:1;">
+<span style="color:#000000;font-family:Arial;font-size:16px;">Vous ne pouvez pas accéder à la racine serveur demandé.</span><span style="color:#000000;font-family:Arial;font-size:13px;"><br></span><span style="color:#000000;font-family:Arial;font-size:15px;"><em>You can not access the requested server root.</em></span></div>
+</div>
+</div>
+</body>
+</html>
