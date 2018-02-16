ebe24f00e82dd9fdbb09dad33913cfae9444003b
AlgostepDG <loic.ab@outlook.fr>
2018-02-16 11:16:36 +0100

    Add files via upload
    32.2

--- mywebos/addeosapps/annonce.php
+++ mywebos/addeosapps/annonce.php
@@ -9,8 +9,8 @@
 </head>
 <body>
 <div id="Layer1" style="position:fixed;overflow:hidden;text-align:left;left:0;top:0;right:0;bottom:0;z-index:1;">
-<div id="wb_Text1" style="position:absolute;left:13px;top:11px;width:348px;height:160px;z-index:0;">
-<span style="color:#000000;font-family:'Trebuchet MS';font-size:15px;"><em>Bonjour à tous !<br><br>Si vous avez des problèmes d'affichages sur votre session vous pouvez essayer de résoudre le problème en vidant le Cache de votre navigateur internet, puis de recharger la page actuelle.<br><br>Bonne année 2018 à tous !</em></span></div>
+<div id="wb_Text1" style="position:absolute;left:13px;top:11px;width:348px;height:120px;z-index:0;">
+<span style="color:#000000;font-family:'Trebuchet MS';font-size:15px;"><em>Bonjour à tous !<br><br>Si vous avez des problèmes d'affichages sur votre session vous pouvez essayer de résoudre le problème en vidant le Cache de votre navigateur internet, puis de recharger la page actuelle.</em></span></div>
 </div>
 </body>
 </html>
---
+++ mywebos/addeosapps/extensions.php
@@ -0,0 +1,43 @@
+<?php
+if (session_id() == "")
+{
+   session_start();
+}
+if (!isset($_SESSION['username']))
+{
+   header('Location: ./../refused.php');
+   exit;
+}
+if (isset($_SESSION['expires_by']))
+{
+   $expires_by = intval($_SESSION['expires_by']);
+   if (time() < $expires_by)
+   {
+      $_SESSION['expires_by'] = time() + intval($_SESSION['expires_timeout']);
+   }
+   else
+   {
+      unset($_SESSION['username']);
+      unset($_SESSION['expires_by']);
+      unset($_SESSION['expires_timeout']);
+      header('Location: ./../refused.php');
+      exit;
+   }
+}
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>RynnaWebOS</title>
+<meta name="generator" content="AlgoStep Company - 2006-2017">
+<link href="RynnaWebOS.css" rel="stylesheet">
+<link href="extensions.css" rel="stylesheet">
+</head>
+<body>
+<div id="Layer1" style="position:fixed;overflow:hidden;text-align:left;left:0;top:0;right:0;bottom:0;z-index:1;">
+<object data="./.config/mimes.php" type="text/html" width="100%" height="100%" style="overflow:auto" ></object>
+</div>
+
+</body>
+</html>
