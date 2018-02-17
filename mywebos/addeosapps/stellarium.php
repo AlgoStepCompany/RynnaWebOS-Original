618a334e683cf3fcecacfb3b6970a4f620b11566
AlgostepDG <loic.ab@outlook.fr>
2018-02-17 12:35:14 +0100

    Add files via upload

---
+++ mywebos/addeosapps/stellarium.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>RynnaWebOS</title>
+<meta name="generator" content="AlgoStep Company - 2006-2017">
+<link href="RynnaWebOS.css" rel="stylesheet">
+<link href="stellarium.css" rel="stylesheet">
+</head>
+<body>
+<div id="Layer1" style="position:fixed;overflow:hidden;text-align:left;left:0;top:0;right:0;bottom:0;z-index:1;">
+
+<object data="http://stellarium.org/fr/" type="text/html" width="100%" height="100%" style="overflow:auto" ></object>
+</div>
+</body>
+</html>
