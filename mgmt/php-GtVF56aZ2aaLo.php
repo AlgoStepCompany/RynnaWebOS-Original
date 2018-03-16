b5a8f75fde94b418a95df32aef67a547490fb97d
AlgostepDG <loic.ab@outlook.fr>
2018-03-16 19:58:41 +0100

    Add files via upload
    Ajustement et protection des fichiers contre les intrusions en cas de connaissance des chemins directe web vers les fichiers.
    Cette protection de variable session avec vérification du nom permet de protéger au mieux votre serveur.

--- mgmt/php-GtVF56aZ2aaLo.php
+++ mgmt/php-GtVF56aZ2aaLo.php
@@ -1,1 +1,45 @@
+<?php
+if (session_id() == "")
+{
+   session_start();
+}
+if (!isset($_SESSION['username']))
+{
+   header('Location: ./index.php');
+   exit;
+}
+if (isset($_SESSION['expires_by']))
+{
+   $expires_by = intval($_SESSION['expires_by']);
+   if (time() < $expires_by)
+   {
+      $_SESSION['expires_by'] = time() + intval($_SESSION['expires_timeout']);
+   }
+   else
+   {
+      unset($_SESSION['username']);
+      unset($_SESSION['expires_by']);
+      unset($_SESSION['expires_timeout']);
+      header('Location: ./index.php');
+      exit;
+   }
+}
+$users = array("root"); // Modifier root par le nom de compte utilisateur qui peut avoir accès à ce fichier ; preferez un Administrateur !
+if (!in_array($_SESSION['username'], $users))
+{
+   header('Location: ./index.php');
+   exit;
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['form_name']) && $_POST['form_name'] == 'logoutform')
+{
+   if (session_id() == "")
+   {
+      session_start();
+   }
+   unset($_SESSION['username']);
+   unset($_SESSION['fullname']);
+   header('Location: ./index.php');
+   exit;
+}
+?>
 <?php phpinfo(); ?>
